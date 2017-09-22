c5f19df8e1efb2173c0716fda931c724bf44ba54
lopezfjose <jflopezfernandez@gmail.com>
2017-09-22 18:29:17 -0400

    Making real progress

---
+++ src/components/video-list.js
@@ -0,0 +1,29 @@
+
+/** Video List
+ *
+ *  Description: [TBD]
+ *
+ *  Author: Jose Fernando Lopez Fernandez
+ *  Date:   22-September-2017
+ *
+ */
+
+import React from 'react';
+
+import VideoListItem from './video-list-item';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={ video.etag } video={ video } />
+  });
+
+  return (
+      <ul className="col-md-4 list-group">
+        { videoItems }
+      </ul>
+  );
+};
+
+
+export default VideoList;
+
--- src/index.js
+++ src/index.js
@@ -8,20 +8,37 @@
  *
  */
 
-const API_KEY = 'AIzaSyDFJBblX90EZRJCTrMrDWWhMlfKlAjObQE';
-
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
-const App = () => {
-  return (
-      <div>
-        <SearchBar />
-      </div>
-  );
-};
+const API_KEY = 'AIzaSyDFJBblX90EZRJCTrMrDWWhMlfKlAjObQE';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch( { key: API_KEY, term: 'motivation' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <p>YouTube Search</p>
+          <SearchBar />
+          <VideoList videos={ this.state.videos } />
+        </div>
+    );
+  }
+}
 
 
 /** Render all of our components onto the web page */
